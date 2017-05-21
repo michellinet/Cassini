b09cdca93180dfbf00954922b545482661040a6f
michellinet <michellinet@procore.com>
2017-05-20 22:39:46 -0700

    Set up ImageViewController.

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Michelline Tran on 5/20/17.
+//  Copyright © 2017 Michelline Tran. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    var imageURL: URL?
+    
+
+}
